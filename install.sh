2ebca81eee84804d34efeeebc4fcc84911351a2f
Ignas Mikalajūnas <ignas.mikalajunas@gmail.com>
2011-12-19 19:08:20 +0200

    Initial import.

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+virtualenv python
+GRROOT=$PWD
+GRPREFIX=$PWD/graph
+GRLIB=$PWD/graph/webapp
+python/bin/pip install --install-option="--prefix=$GRPREFIX" --install-option="--install-lib=$GRLIB" graphite-web
+python/bin/pip install --install-option="--prefix=$GRPREFIX" --install-option="--install-lib=$GRLIB" carbon
+python/bin/pip install --install-option="--prefix=$GRPREFIX" --install-option="--install-lib=$GRLIB" whisper
+python/bin/pip install --install-option="--prefix=$GRPREFIX" --install-option="--install-lib=$GRLIB" gunicorn
+cd graph/webapp/graphite
+$GRROOT/python/bin/python manage.py syncdb
+cd $GRROOT
+cp $GRPREFIX/conf/carbon.conf.example $GRPREFIX/conf/carbon.conf
+cp $GRPREFIX/conf/storage-schemas.conf.example $GRPREFIX/conf/storage-schemas.conf
